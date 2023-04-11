9acc49704c4f67a73633df8fb068a1a7c8197e10
Adrian Pusana <aguilaradrian423@gmail.com>
2023-04-11 11:39:28 +0800

    initial testing for arch

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('test', function (){
+    $response = Http::get("http://export.arxiv.org/api/query?search_query=all:python");
+    $xml = new SimpleXMLElement($response->body());
+    $info = [];
+    foreach ($xml->entry as $item) {
+        $authors = [];
+        foreach ($item->author as $author) {
+            $authors[] = (string) $author->name;
+        }
+        $links = [];
+        foreach ($item->link as $link) {
+            $links[] = [
+                'href' => (string) $link['href'],
+                'type' => (string) $link['type'],
+            ];
+        }
+        $info[] = [
+            'title' => (string) $item->title,
+            'summary' => (string) $item->summary,
+            'published' => (string) $item->published,
+            'updated' => (string) $item->updated,
+            'authors' => $authors,
+            'links' => $links,
+        ];
+    }
+    // return response()->json($info);
+    return view()->make('test')->with(['info' => $info]);
 });
